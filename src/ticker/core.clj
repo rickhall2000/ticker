91779f27a3b0e7a2f79a0b0e8d6913775b1d359d
Rick Hall <rickhall2000@hotmail.com>
2014-03-28 11:34:08 -0400

    added random prices, times to ticker, created stock list

--- src/ticker/core.clj
+++ src/ticker/core.clj
@@ -3,19 +3,46 @@
              :refer [chan <! >! timeout go]
               :as async]))
 
-(defn make-ticker [symbol t]
+(defn adjust-price [old-price]
+  (let  [numerator (- (rand-int 30) 15)
+         adjustment (* numerator 0.01M)]
+    (+ old-price adjustment)))
+
+(defn new-transaction [symbol price]
+  {:symbol symbol
+;;   :time (java.util.Date.) I will want this later
+   :price price})
+
+(defn random-time [t]
+  (* t (+ 1 (rand-int 5))))
+
+(defn make-ticker [symbol t start-price]
   (let [c (chan)]
     (go
-     (while true
-       (<! (timeout t))
-       (>! c {:symbol symbol})))
+     (loop [price start-price]
+       (let [new-price (adjust-price price)]
+         (do
+           (<! (timeout (random-time t)))
+           (>! c (new-transaction symbol new-price))
+           (recur new-price)))))
     c))
 
+(def stocks [ ;; symbol min-interval starting-price
+             ["AAPL" 1400 537 ]
+             ["AMZN" 4200 345]
+             ["CSCO" 400  22]
+             ["EBAY" 1200 55]
+             ["GOOG" 8200 1127]
+             ["IBM" 2200  192]
+             ["MSFT" 500 40]
+             ["ORCL" 1000 39]
+             ["RHT" 10200  53]
+             ["T" 600 35]])
+
 (defn run-sim []
-  (let [ticker (make-ticker "Tick" 2000)]
+  (let [ticker (async/merge (map #(apply make-ticker %) stocks)) ]
     (go
      (loop [x 0]
-       (when (< x 5)
-         (do
-           (println (<! ticker))
-           (recur (inc x))))))))
+       (when (< x 1000)
+         (do (println (str x "-" (<! ticker)))
+             (recur (inc x))))))))
