afa35e8bd7bfc98f1a60e3f14295be4f28469f9d
Rick Hall <rickhall2000@hotmail.com>
2014-03-28 09:07:22 -0400

    created a make-ticker function that can put items on a channel at regular intervals

--- src/ticker/core.clj
+++ src/ticker/core.clj
@@ -1,2 +1,21 @@
 (ns ticker.core
-  (:require [clojure.core.async :as async]))
+  (:require [clojure.core.async
+             :refer [chan <! >! timeout go]
+              :as async]))
+
+(defn make-ticker [symbol t]
+  (let [c (chan)]
+    (go
+     (while true
+       (<! (timeout t))
+       (>! c {:symbol symbol})))
+    c))
+
+(defn run-sim []
+  (let [ticker (make-ticker "Tick" 2000)]
+    (go
+     (loop [x 0]
+       (when (< x 5)
+         (do
+           (println (<! ticker))
+           (recur (inc x))))))))
